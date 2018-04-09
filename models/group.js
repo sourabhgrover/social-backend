1b34c224f80b40cf35867af13a22c1015626fbfb
sourabh.grover <Sourabh.Grover@kibocommerce.com>
2018-04-09 18:30:17 +0530

    Change Structure of Group Table

--- models/group.js
+++ models/group.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   var group = sequelize.define('group', {
     group_url: DataTypes.STRING,
     name: DataTypes.STRING,
-    img_url: DataTypes.STRING,
+    img_name: DataTypes.STRING,
     platform_id: DataTypes.INTEGER
   }, {});
   group.associate = function(models) {
