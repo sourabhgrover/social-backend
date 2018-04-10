4d04790b4f22851217ecde512a4c6f228d3e14be
sourabh.grover <Sourabh.Grover@kibocommerce.com>
2018-04-10 18:37:21 +0530

    POST Request for Group

--- controllers/group.controller.js
+++ controllers/group.controller.js
@@ -1,4 +1,5 @@
 const Group = require('../models/').group;
+const GroupRelation = require('../models/').group_relation;
 
 var groupController = {
 
@@ -6,7 +7,7 @@ var groupController = {
         return Group
             .findAll()
                 .then(Group => {
-               // projects will be an array of all Project instances
+                    // projects will be an array of all Project instances
                      res.status(200).send(Group)
          }).catch(
                 error => {
@@ -15,7 +16,35 @@ var groupController = {
                     }
             );
     },
+    filter(req,res){
+        let queryString = req.query;
+        platform_id = queryString.platform_id;
 
+        whereConditionObj =  {};
+        
+        if(queryString.id){
+            whereConditionObj.id = queryString.id;
+        }
+        if(queryString.platform_id){
+            whereConditionObj.platform_id = queryString.platform_id;
+        }
+        
+        return Group
+        .findAll({
+                where : whereConditionObj
+            })
+            .then(Group => {
+                // projects will be an array of all Project instances
+                 res.status(200).send(Group)
+     }).catch(
+            error => {
+                        console.log(error.stack);
+                        res.status(400).send(error)
+                }
+        );
+        
+    },
+    // TODO Refactor
     post(req,res){
    
         // get the name from request body
@@ -23,22 +52,44 @@ var groupController = {
         if(!name){
             res.status(400).send();
         }
+
+         // get the name from request body
+         let group_url = req.body.group_url;
+         if(!group_url){
+             res.status(400).send();
+         }
         
         // get the Image Url from the request body
-        let img_url = req.body.img_url;
+        let img_name = req.body.img_name;
         
         // Create new job object
         let GroupData = {
                 'name' : name,
-                'img_url' : img_url
+                'img_name' : img_name,
+                'group_url' : group_url,
+                'platform_id' : req.body.platform_id
             };
 
         return Group
-                .create({
-                    name : req.body.name,
-                    img_url : req.body.img_url
-                })
-                .then(Group => res.status(201).send(Group))
+                .create(GroupData)
+                .then(
+                        Group => {
+                        
+                            let GroupRelationData = [];
+                            for (let i = 0, len = req.body.category_id.length; i < len; i++) {
+                                singleGroupRelation = {
+                                    'group_id' : Group.id,
+                                    'category_id' : req.body.category_id[i],
+                                    'platform_id' : Group.platform_id
+                                }
+                                GroupRelationData.push(singleGroupRelation);   
+                              }
+                            
+                            GroupRelation.bulkCreate(GroupRelationData,  {individualHooks: true }),
+                            // TODO Id is not returning in Group Relation Data
+                            res.status(201).send({Group,GroupRelationData})
+                               })
+
                 .catch(error => res.status(400).send(error));
     },
 
