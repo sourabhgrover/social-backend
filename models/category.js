6ed0f91ac086eca5a12fcb2dc861f168049f1456
sourabh.grover <Sourabh.Grover@kibocommerce.com>
2018-04-19 16:59:40 +0530

    Add Validation for Platform and Category POST Request Add Uniq

--- models/category.js
+++ models/category.js
@@ -1,8 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var category = sequelize.define('category', {
-    name: DataTypes.STRING,
-    img_name: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        len: {
+            args: 3,
+            msg: "Category Name must be atleast 3 characters in length"
+        }
+    }
+    },
+    img_name: DataTypes.STRING,
   }, {});
   category.associate = function(models) {
     // associations can be defined here
--- models/platform.js
+++ models/platform.js
@@ -1,11 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var platform = sequelize.define('platform', {
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        len: {
+            args: 3,
+            msg: "Platform Name must be atleast 3 characters in length"
+        }
+    }
+    },
     img_name: DataTypes.STRING,
     package_name_android: DataTypes.STRING,
     package_name_ios: DataTypes.STRING,
   }, {});
+
   platform.associate = function(models) {
     // associations can be defined here
   };
