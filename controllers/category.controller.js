f8388f4a9eb9d29cda53a0ad84f367947116a726
Sourabh Grover <sourabhgrover29@gmail.com>
2018-05-12 19:24:20 +0530

    Edit API For Category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -34,7 +34,6 @@ var CategoryController = {
                  res.status(200).send(Category)
      }).catch(
         error => res.status(400).send(error));
-
     },
     post(req,res){
         
@@ -45,79 +44,36 @@ var CategoryController = {
             res.status(400).send();
         }
         categoryData['name'] = name;
-        
-
-         // Regular expression for image type:
-        // This regular image extracts the "jpeg" from "image/jpeg"
-        var imageTypeRegularExpression      = /\/(.*?)$/;      
-
-        // Generate random string
-        var crypto                          = require('crypto');
-        var seed                            = crypto.randomBytes(20);
-        var uniqueSHA1String                = crypto
-                                               .createHash('sha1')
-                                                .update(seed)
-                                                 .digest('hex');
 
         let img_name = req.body.img_name;
         if(img_name){
-            var imageBuffer                      = decodeBase64Image(img_name);
-            var userUploadedPlatformImageLocation = 'public/images/category/';
-    
-            var uniqueRandomImageName            = 'CategoryImage-' + uniqueSHA1String;
-            // This variable is actually an array which has 5 values,
-            // The [1] value is the real image extension
-            var imageTypeDetected                = imageBuffer
-                                                    .type
-                                                     .match(imageTypeRegularExpression);
-    
-            var userUploadedImagePath            = userUploadedPlatformImageLocation + 
-                                                   uniqueRandomImageName +
-                                                   '.' + 
-                                                   imageTypeDetected[1];
-            let uploadedImageName =     uniqueRandomImageName + '.' + imageTypeDetected[1];
-    
-             // Save decoded binary image to disk
-             try
-             {
-             fs.writeFile(
-                 userUploadedImagePath,
-                 imageBuffer.data,  
-                 {encoding: 'base64'},   
-                 function(err) {
-                     if(err){
-                        return res.status(400).send(err);
-                      } 
-                    }
-                );
-             }
-             catch(error)
-             {
-                 console.log('ERROR:', error);
-             }
-             categoryData['img_name'] = uploadedImageName;
+            categoryData['img_name'] =  uploadImage(img_name);
         }
-
-        function decodeBase64Image(dataString) 
-        {
-          var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-          var response = {};
-    
-          if (matches.length !== 3) 
-          {
-            return res.status(400).send("Invalid Input String");
-          }
-    
-          response.type = matches[1];
-          response.data = new Buffer(matches[2], 'base64');
-    
-          return response;
-        }
-
         return Category
                 .create(categoryData)
                 .then(Category => res.status(201).send(Category))
                 .catch(error => res.status(400).send(error));
+    },
+    put(req,res){
+        
+        let categoryData = {};
+        // get the name from request body
+        let name = req.body.name;
+        if(!name){
+            res.status(400).send();
+        }
+        categoryData['name'] = name;
+
+        let img_name = req.body.img_name;
+        if(img_name){
+            categoryData['img_name'] =  uploadImage(img_name);
+        }
+
+                return Category.update(
+                    categoryData,
+                    { where: { id: req.params.id } }
+                  ).then(Category => res.status(200).send(Category))
+                  .catch(error => res.status(400).send(error));
     },
     delete(req, res) {
         return Category
@@ -136,5 +92,75 @@ var CategoryController = {
           .catch(error => res.status(400).send(error));
       }
 };
+
+
+function uploadImage(img_name){
+
+    
+    // Regular expression for image type:
+    // This regular image extracts the "jpeg" from "image/jpeg"
+    var imageTypeRegularExpression      = /\/(.*?)$/;      
+
+    // Generate random string
+    var crypto                          = require('crypto');
+    var seed                            = crypto.randomBytes(20);
+    var uniqueSHA1String                = crypto
+                                           .createHash('sha1')
+                                            .update(seed)
+                                             .digest('hex');
+
+var imageBuffer                      = decodeBase64Image(img_name);
+var userUploadedPlatformImageLocation = 'public/images/category/';
+    
+            var uniqueRandomImageName            = 'CategoryImage-' + uniqueSHA1String;
+// This variable is actually an array which has 5 values,
+// The [1] value is the real image extension
+var imageTypeDetected                = imageBuffer
+                                        .type
+                                         .match(imageTypeRegularExpression);
+
+var userUploadedImagePath            = userUploadedPlatformImageLocation + 
+                                       uniqueRandomImageName +
+                                       '.' + 
+                                       imageTypeDetected[1];
+let uploadedImageName =     uniqueRandomImageName + '.' + imageTypeDetected[1];
+
+ // Save decoded binary image to disk
+ try
+ {
+ fs.writeFile(
+     userUploadedImagePath,
+     imageBuffer.data,  
+     {encoding: 'base64'},   
+     function(err) {
+         if(err){
+            return res.status(400).send(err);
+          } 
+        }
+    );
+ }
+ catch(error)
+ {
+     console.log('ERROR:', error);
+ }
+ return uploadedImageName;
+
+}
+
+function decodeBase64Image(dataString) 
+{
+var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+var response = {};
+
+if (matches.length !== 3) 
+{
+return res.status(400).send("Invalid Input String");
+}
+
+response.type = matches[1];
+response.data = new Buffer(matches[2], 'base64');
+
+return response;
+}
 
 module.exports = CategoryController;
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -8,6 +8,7 @@ router.get('/',category.get);
 router.get('/:id',category.getOne);
 router.post('/',category.post);
 router.delete('/:id',category.delete);
+router.put('/:id',category.put);
 
 
 module.exports = router;
